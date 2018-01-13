1527b041ae2a252365955358f14cff2841a6f605
kalyanishere <bkalyan80@gmail.com>
2018-01-13 20:55:02 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -42,3 +42,20 @@ app.get('/Main*' , function(req,res) {
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+/*Allow CORS*/
+app.use(function(req, res, next) {
+	 
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+	res.setHeader('Access-Control-Max-Age', '1000');
+	  
+	next();
+});
+var options = {
+  key: fs.readFileSync('./key.pem', 'utf8'),
+  cert: fs.readFileSync('./server.crt', 'utf8')
+};
+   
+https.createServer(options, app).listen(8081);
